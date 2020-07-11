4b2158ff0af8c1d6da35c3f120778d8b884a86e1
Xuan Hien <hiennguyen9874@gmail.com>
2020-07-11 16:14:16 +0000

    fix multi lr on diffirent layer

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -6,27 +6,29 @@ def build_optimizers(config, model):
     if 'specified_lr' in cfg_optimizer and len(cfg_optimizer['specified_lr']) > 0:
         specified_lr = True
         base_params = []
+        new_params = []
         for name, module in model.named_children():
             if name not in cfg_optimizer['specified_lr']:
                 base_params += [p for p in module.parameters()]
-        param_groups = [{'params': base_params}]
-        for key, value in cfg_optimizer['specified_lr'].items():
-            param_groups.append({'params': getattr(model, key).parameters(), 'lr': value})
+            else:
+                new_params += [p for p in module.parameters()]
+        param_groups = [{'params': base_params},
+                        {'params': new_params, 'lr': cfg_optimizer['lr']}]
     else:
         param_groups = model.parameters()
         
     if cfg_optimizer['name'] == 'adam':
         dict_paramsters = {
-            'lr': cfg_optimizer['lr'],
+            'lr': cfg_optimizer['lr']*0.1,
             'weight_decay': cfg_optimizer['weight_decay'],
             'beta1': cfg_optimizer['adam_beta1'],
             'beta2': cfg_optimizer['adam_beta2']
         }
         if specified_lr:
-            dict_paramsters.update(dict(cfg_optimizer['specified_lr']))
+            dict_paramsters.update({"specified_lr": cfg_optimizer['specified_lr']})
         return optim.Adam(
             param_groups,
-            lr=cfg_optimizer['lr'],
+            lr=cfg_optimizer['lr']*0.1,
             weight_decay=cfg_optimizer['weight_decay'],
             betas=(cfg_optimizer['adam_beta1'],
                     cfg_optimizer['adam_beta2'])), dict_paramsters
@@ -40,7 +42,7 @@ def build_optimizers(config, model):
             'nesterov': cfg_optimizer['sgd_nesterov']
         }
         if specified_lr:
-            dict_paramsters.update(cfg_optimizer['specified_lr'])
+            dict_paramsters.update({"specified_lr": cfg_optimizer['specified_lr']})
         return optim.SGD(
             param_groups,
             lr=cfg_optimizer['lr'],
