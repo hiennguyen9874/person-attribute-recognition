b4371e378f4a519d984ccf4cca3764e348f840e0
Xuan Hien <hiennguyen9874@gmail.com>
2020-07-11 08:16:34 +0000

    fix model baseline_attribute

--- models/baseline_attribute.py
+++ models/baseline_attribute.py
@@ -35,15 +35,30 @@ class BaselineAttribute(nn.Module):
         super(BaselineAttribute, self).__init__()
         self.num_classes = num_classes
         self.base = torchvision.models.resnet50(pretrained=True)
-        
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+
+        # remove the final downsample of resnet
+        # self.base.layer4[0].downsample[0].stride = (1, 1)
+        # self.base.layer4[0].conv2.stride=(1,1)
+
         self.classifier = nn.Sequential(
             nn.Linear(2048, self.num_classes),
             nn.BatchNorm1d(self.num_classes)
         )
-        self.classifier.apply(weights_init_classifier)
+        # self.classifier.apply(weights_init_classifier)
 
     def forward(self, x):
-        x = self.base(x)
+        x = self.base.conv1(x)
+        x = self.base.bn1(x)
+        x = self.base.relu(x)
+        x = self.base.maxpool(x)
+
+        x = self.base.layer1(x)
+        x = self.base.layer2(x)
+        x = self.base.layer3(x)
+        x = self.base.layer4(x)
+        # x.size = (batch_size, 2048, 16, 8)
+        x = self.avgpool(x)
         x = x.view(x.shape[0], -1)
         # x.size() = (batch_size, 2048)
         x = self.classifier(x)
