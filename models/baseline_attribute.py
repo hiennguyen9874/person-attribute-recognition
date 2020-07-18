5450cc9f5470d581322ee83178781a2a4d9acd8b
Xuan Hien <hiennguyen9874@gmail.com>
2020-07-18 14:02:07 +0000

    add weight_init, build_backbone, build_aggregation,

--- models/baseline_attribute.py
+++ models/baseline_attribute.py
@@ -7,25 +7,18 @@ import sys
 sys.path.append('.')
 
 from utils import summary
-from . import GeneralizedMeanPoolingP
+from .layer import GeneralizedMeanPoolingP
+from .build_backbone import build_backbone
+from .weight_init import weights_init_classifier, weights_init_kaiming
 
 class BaselineAttribute(nn.Module):
     ''' https://arxiv.org/pdf/2005.11909.pdf
     '''
-    __model_factory = {
-        'resnet50': torchvision.models.resnet50,
-        'resnet101': torchvision.models.resnet101
-    }
     def __init__(self, num_classes, backbone='resnet50', last_stride_1=True, pretrained=True):
         super(BaselineAttribute, self).__init__()
         self.num_classes = num_classes
         
-        resnet = self.__model_factory[backbone](pretrained=pretrained)
-        if last_stride_1:
-            # remove the final downsample of resnet
-            resnet.layer4[0].downsample[0].stride = (1, 1)
-            resnet.layer4[0].conv2.stride=(1,1)
-
+        resnet = build_backbone(backbone, pretrained, last_stride_1)
         self.base = nn.Sequential(
             resnet.conv1,
             resnet.bn1,
@@ -37,13 +30,13 @@ class BaselineAttribute(nn.Module):
             resnet.layer4
         )
 
-        # self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.avgpool = GeneralizedMeanPoolingP()
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
 
-        self.classifier = nn.Sequential(
-            nn.Linear(2048, self.num_classes),
-            nn.BatchNorm1d(self.num_classes)
-        )
+        self.linear = nn.Linear(2048, self.num_classes)
+        self.bn = nn.BatchNorm1d(self.num_classes)
+
+        self.linear.apply(weights_init_classifier)
+        self.bn.apply(weights_init_kaiming)
 
     def forward(self, x):
         x = self.base(x)
@@ -51,7 +44,8 @@ class BaselineAttribute(nn.Module):
         x = self.avgpool(x)
         x = x.view(x.shape[0], -1)
         # x.size() = (batch_size, 2048)
-        x = self.classifier(x)
+        x = self.linear(x)
+        x = self.bn(x)
         return x
     
     def get_heat_maps(self, x, return_output=True):
@@ -68,7 +62,8 @@ class BaselineAttribute(nn.Module):
         if return_output:
             x = self.avgpool(x)
             x = x.view(x.shape[0], -1)
-            x = self.classifier(x)
+            x = self.linear(x)
+            x = self.bn(x)
             return x, heatmaps
         return heatmaps
 
