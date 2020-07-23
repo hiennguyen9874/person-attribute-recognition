6546b0e86a27d03b5a15a61f82cdb3ba5b67a4d0
Xuan Hien <hiennguyen9874@gmail.com>
2020-07-23 09:25:46 +0700

    Add head

--- models/util.py
+++ models/util.py
@@ -4,11 +4,10 @@ __all__ = ['get_norm']
 
 def get_norm(in_features, type_norm='2d', bias_freeze=False):
     assert type_norm in ['1d', '2d']
-    norm = None
     if type_norm == '1d':
         norm = nn.BatchNorm1d(in_features)
     elif type_norm == '2d':
-        norm == nn.BatchNorm2d(in_features)
+        norm = nn.BatchNorm2d(in_features)
     if bias_freeze:
         norm.bias.requires_grad_(False)
     return norm
