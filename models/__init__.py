b6bd8fedf201e3a5f029103158b28c5843495e9c
Xuan Hien <hiennguyen9874@gmail.com>
2020-07-24 01:06:04 +0000

    asssert optimizers

--- models/__init__.py
+++ models/__init__.py
@@ -5,10 +5,8 @@ from .util import *
 
 def build_model(config, num_classes, device=torch.device('cpu')):
     dict_paramsters = None
-    if config['name'] == 'osnet':
-        model = OSNet(num_classes=num_classes)
-
-    elif config['name'] == 'baseline':
+    
+    if config['name'] == 'baseline':
         dict_paramsters = {
             'backbone': config['backbone'],
             'pretrained': config['pretrained'],
