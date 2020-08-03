bc189e4a327587d98c7ec86bdf5a31f996a6a07e
Xuan Hien <hiennguyen9874@gmail.com>
2020-08-03 08:08:36 +0000

    Update config for episode training

--- losses/Singular_BCE.py
+++ losses/Singular_BCE.py
@@ -6,8 +6,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Singular_BCE(nn.Module):
-    def __init__(self):
+    def __init__(self, reduction='sum'):
         super(Singular_BCE, self).__init__()
+        self.reduction = reduction
 
     def forward(self, logits, targets, idx_attribute):
-        return F.binary_cross_entropy_with_logits(logits.gather(1, idx_attribute.unsqueeze(1)).squeeze(), targets.gather(1, idx_attribute.unsqueeze(1)).squeeze())
+        return F.binary_cross_entropy_with_logits(logits.gather(1, idx_attribute.unsqueeze(1)).squeeze(), targets.gather(1, idx_attribute.unsqueeze(1)).squeeze(), reduction=self.reduction)
