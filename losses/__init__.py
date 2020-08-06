5e507a70054fd2e2cf6ee8cb165f80c75bfc5478
Xuan Hien <hiennguyen9874@gmail.com>
2020-08-06 23:31:19 +0000

    Add reduction loss

--- losses/__init__.py
+++ losses/__init__.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from losses.CE_loss import CEL_Sigmoid
 from losses.Singular_BCE import Singular_BCE
 
-def build_losses(config, pos_ratio, use_gpu=True):
+def build_losses(config, pos_ratio, num_attribute, use_gpu=True):
     cfg_loss = config['loss']
     if cfg_loss['name'] == 'BCEWithLogitsLoss':
         pos_weight = torch.exp(-1 * pos_ratio)
@@ -16,7 +16,7 @@ def build_losses(config, pos_ratio, use_gpu=True):
     elif cfg_loss['name'] == 'CEL_Sigmoid':
         return CEL_Sigmoid(pos_ratio, use_gpu=use_gpu, reduction=cfg_loss['reduction']), {'reduction': cfg_loss['reduction']}
     elif cfg_loss['name'] == 'Singular_BCE':
-        return Singular_BCE(reduction=cfg_loss['reduction']), {'reduction': cfg_loss['reduction']}
+        return Singular_BCE(num_attribute, reduction=cfg_loss['reduction']), {'reduction': cfg_loss['reduction']}
     else:
         raise KeyError('config[loss][name] error')
 
