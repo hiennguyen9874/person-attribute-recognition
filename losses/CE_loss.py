37dfed98abfe9cd68a546dedf2770f72117da05f
Xuan Hien <hiennguyen9874@gmail.com>
2020-08-13 16:05:00 +0700

    Update CEL_Loss

--- losses/CE_loss.py
+++ losses/CE_loss.py
@@ -5,14 +5,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def ratio2weight(targets, ratio):
-    ratio = ratio.type_as(targets)
-    weights = torch.exp(targets * (1 - ratio) + (1 - targets) * ratio)
-
-    # # for RAP dataloader, targets element may be 2, with or without smooth, some element must great than 1
-    # weights[targets > 1] = 0.0
-
-    return weights
 
 class CEL_Sigmoid(nn.Module):
     r""" https://arxiv.org/pdf/2005.11909.pdf
@@ -28,7 +20,12 @@ class CEL_Sigmoid(nn.Module):
         batch_size = logits.shape[0]
         loss = F.binary_cross_entropy_with_logits(logits, targets, reduction='none')
         if self.pos_ratio is not None:
-            weight = ratio2weight(targets, self.pos_ratio)
+            weight = self.__ratio2weight(targets, self.pos_ratio)
             loss = (loss * weight)
         loss = loss.sum() / batch_size if self.reduction == 'mean' else loss.sum()
         return loss
+
+    def __ratio2weight(self, targets, ratio):
+        ratio = ratio.type_as(targets)
+        weights = torch.exp(targets * (1 - ratio) + (1 - targets) * ratio)
+        return weights
