26ce8ca3eddbf09de8f2c7f18c41277eac8e6a9d
Xuan Hien <hiennguyen9874@gmail.com>
2020-09-14 02:09:04 +0700

    Update loss

--- losses/Focal_loss.py
+++ losses/Focal_loss.py
@@ -6,6 +6,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+    
+@torch.jit.script
+def _ratio2weight(targets, ratio):
+    ratio = ratio.type_as(targets)
+    weights = torch.exp(targets * (1 - ratio) + (1 - targets) * ratio)
+    return weights
+
 class FocalLoss(nn.Module):
     def __init__(self, pos_ratio=None, alpha=1, gamma=2, reduction='mean', **kwargs):
         super(FocalLoss, self).__init__()
@@ -19,16 +26,11 @@ class FocalLoss(nn.Module):
         batch_size = inputs.shape[0]
         loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
         pt = torch.exp(-loss)
-        loss = self.alpha * (1-pt)**self.gamma * loss
+        alpha = targets * self.alpha + (1 - targets) * (1 - self.alpha)
+        loss = alpha * (1-pt)**self.gamma * loss
         
         if self.pos_ratio is not None:
-            weight = self.__ratio2weight(targets, self.pos_ratio)
+            weight = _ratio2weight(targets, self.pos_ratio)
             loss = (loss * weight)
         loss = loss.sum() / batch_size if self.reduction == 'mean' else loss.sum()
         return loss
-    
-    @torch.jit.script
-    def __ratio2weight(self, targets, ratio):
-        ratio = ratio.type_as(targets)
-        weights = torch.exp(targets * (1 - ratio) + (1 - targets) * ratio)
-        return weights
