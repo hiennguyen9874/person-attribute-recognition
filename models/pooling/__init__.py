baf6520902e351be70badca8e3e4a313342d7588
Xuan Hien <hiennguyen9874@gmail.com>
2020-09-14 16:30:12 +0700

    Add max pooling

--- models/pooling/__init__.py
+++ models/pooling/__init__.py
@@ -2,10 +2,12 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
 
+import torch.nn as nn
+
 from models.pooling.gem_pooling import GeneralizedMeanPoolingP
 from models.pooling.avg_pooling import AvgPooling2d
 
-__poolings = {'gem_pooling': GeneralizedMeanPoolingP, 'avg_pooling': AvgPooling2d}
+__poolings = {'gem_pooling': GeneralizedMeanPoolingP, 'avg_pooling': AvgPooling2d, 'max_pooling': nn.AdaptiveMaxPool2d}
 
 def build_pooling(name, pooling_size=1):
     if name not in list(__poolings.keys()):
