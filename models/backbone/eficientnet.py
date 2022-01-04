66cf65d8c7cede923f1ccf5f5bf6f291ec8c2900
Xuan Hien <hiennguyen9874@gmail.com>
2022-01-04 02:35:23 +0000

    Fix package and chmod

--- models/backbone/eficientnet.py
+++ models/backbone/eficientnet.py
@@ -1,32 +1,36 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", ".."))
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from efficientnet_pytorch import EfficientNet
 
 from utils import summary
 
+
 class Efficient(nn.Module):
-    def __init__(self, name='efficientnet-b5', advprop=True):
+    def __init__(self, name="efficientnet-b5", advprop=True):
         super(Efficient, self).__init__()
+        from efficientnet_pytorch import EfficientNet
+
         self.name = name
         self.model = EfficientNet.from_pretrained(name, advprop=advprop)
-        
+
     def forward(self, x):
         return self.model.extract_features(x)
-    
+
     def get_out_channels(self):
         return self.model._bn1.num_features
-    
+
     def get_image_size(self):
         return EfficientNet.get_image_size(self.name)
 
+
 if __name__ == "__main__":
-    name = 'efficientnet-b5'
+    name = "efficientnet-b5"
     model = EfficientNet.from_pretrained(name, advprop=True)
     print("Input image size: ", EfficientNet.get_image_size(name))
     # summary(model, input_data=(3, EfficientNet.get_image_size(name), EfficientNet.get_image_size(name)//2))
--- models/backbone/timm_extractor.py
+++ models/backbone/timm_extractor.py
@@ -1,13 +1,17 @@
-import timm
 import torch
 import torch.nn as nn
+
 
 class TimmModel(nn.Module):
     def __init__(self, name, pretrained=True):
         super(TimmModel, self).__init__()
+        import timm
+
         self.feature_extractor = timm.create_model(name, pretrained=pretrained)
-        
-        self.feature_dim = self.feature_extractor.forward_features(torch.randn(2, 3, 256, 128)).size(1)
+
+        self.feature_dim = self.feature_extractor.forward_features(
+            torch.randn(2, 3, 256, 128)
+        ).size(1)
 
     def forward(self, x):
         return self.feature_extractor.forward_features(x)
